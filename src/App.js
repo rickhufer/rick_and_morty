454b06204d94021c752ec79928110642b9db157b
Ricardo <rickhufer@outlook.com>
2023-03-13 11:01:18 -0500

    Mediante API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
-import characters from './data.js'
+// import characters from './data.js'
 import Cards from './components/Cards/Cards';
 import SearchBar from './components/SearchBar/SearchBar';
+import { useState } from 'react';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  const onSearch = (id) => {
+    const URL_BASE = "https://be-a-rym.up.railway.app/api";
+    const KEY = "b682d44ea194.e61171acf8c72545c21e";
+
+    if (characters.find(
+      (elem) => elem.id === id)
+    ) return alert("Personaje repetido");
+
+    fetch(`${URL_BASE}/character/${id}?key=${KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.name) {
+          setCharacters((oldChar) => [...oldChar, data]);
+        } else {
+          window.alert('No hay personajes con ese ID');
+        }
+      });
+  }
+
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar onSearch={onSearch} />
       <Cards characters={characters} />
     </div>
   );
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import styles from './SearchBar.module.css'
 
-function SearchBar(onSearch) {
+function SearchBar({ onSearch }) {
+  const [id, setId] = useState("");
+
+  const inputText = (event) => {
+    setId(event.target.value);
+  }
+
   return (
     <div className={styles.search}>
-      <input className={styles.input} type="text" />
-      <button className={styles.button} onClick={(elem) => onSearch(elem)}>Agregar</button>
+      <input className={styles.input} type="text" onChange={inputText} />
+      <button className={styles.button} onClick={() => { onSearch(id) }}>Agregar</button>
     </div>
   );
 }
