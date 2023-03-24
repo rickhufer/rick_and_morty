3d1b4ebb4d6c41c2eab612189751c6a22c10dcea
Ricardo Huallpayunca <rickhufer@outlook.com>
2023-03-24 10:28:40 -0500

    Con favoritos pero sin la página favoritos

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,42 @@ import styles from "./Card.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { connect } from "react-redux"
+import { addFavorite, removeFavorite } from "../../redux/actions";
+import React from "react";
 
-const Card = ({ id, name, species, gender, image, onClose }) => {
+const Card = ({ id, name, species, gender, image, onClose, myFavorites, addFavorite, removeFavorite }) => {
+
+  const [isFav, setIsFav] = useState(false);
+
+  const handleFavorite = () => {
+    if (isFav) {
+      setIsFav(false);
+      removeFavorite(id);
+    } else {
+      setIsFav(true);
+      addFavorite({
+        id,
+        name,
+        species,
+        gender,
+        image,
+        onClose,
+        addFavorite,
+        removeFavorite,
+      });
+    }
+  };
+
+  useEffect(() => {
+    myFavorites.forEach((fav) => {
+      if (fav.id === id) {
+        setIsFav(true);
+      }
+    });
+  }, [myFavorites]);
+
   return (
     <div className={styles.card}>
       <Link className={styles.detailLink} to={`/detail/${id}`}>
@@ -16,11 +50,24 @@ const Card = ({ id, name, species, gender, image, onClose }) => {
       <h2 className={styles.gender}><b>Gender:</b> {gender}</h2>
 
       <div className={styles.buttons}>
-        <button className={styles.button}><FontAwesomeIcon className={styles.buttonLike} icon={faHeart} size="2x" /></button>
+        {isFav ? (<button onClick={handleFavorite} className={styles.button}><FontAwesomeIcon className={styles.buttonLikeEnabled} icon={faHeart} size="2x" /></button>) : <button onClick={handleFavorite} className={styles.button}><FontAwesomeIcon className={styles.buttonLike} icon={faHeart} size="2x" /></button>}
         <button className={styles.button} onClick={() => onClose(id)}><FontAwesomeIcon className={styles.buttonClose} icon={faCircleXmark} size="2x" /></button>
       </div>
     </div>
   )
 }
 
-export default Card;
+const mapStateToProps = (state) => {
+  return {
+    myFavorites: state.myFavorites,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addFavorite: (character) => { dispatch(addFavorite(character)) },
+    removeFavorite: (id) => { dispatch(removeFavorite(id)) }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Card);
---
+++ src/redux/reducer.js
@@ -0,0 +1,31 @@
+import {
+  ADD_FAVORITE,
+  REMOVE_FAVORITE,
+} from "./actions";
+
+const initialState = {
+  myFavorites: [],
+};
+
+const rootReducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case ADD_FAVORITE:
+      return {
+        ...state,
+        myFavorites: [...state.myFavorites, action.payload]
+      };
+
+    case REMOVE_FAVORITE:
+      return {
+        ...state,
+        myFavorites: state.myFavorites.filter((elem) => elem.id !== action.payload),
+      };
+
+    default:
+      return { ...state }
+  }
+
+};
+
+export default rootReducer;
